04e7c7cc7529380623f1a54e53a0cc2223407087
Levi-Jordan <Levijordananderson@gmail.com>
2025-06-09 18:28:30 -0600

    push

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";\
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul style={{
+            color: "orange",
+            display: "flex",
+            justifyContent: "space-around"
+        }}>
+
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/login">Sign In</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
