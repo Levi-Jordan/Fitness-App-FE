462a2c00206499ded1ba1ecc458963e442a4d765
Levi-Jordan <Levijordananderson@gmail.com>
2025-06-12 19:03:11 -0600

    Login and Register Form

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,2 +1,41 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from '../../context/AuthContext';
+import api from '../../utility/api';
+
+export default function LoginForm() {
+    const [form, setForm] = useState({ username: '', email: '', password: '' });
+    const { login } = useAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await api.post('/user/login', form);
+            login(res.data.token);
+            navigate('/dashboard')
+        } catch (err) {
+            console.error(err)
+            alert("Login failed")
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                name="email"
+                placeholder="email"
+                value={form.email}
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                required />
+            <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+                required />
+            <button type="submit">Log in!</button>
+        </form>
+    )
+}
--- src/components/Forms/RegisterForm.jsx
+++ src/components/Forms/RegisterForm.jsx
@@ -1,32 +1,48 @@
 import { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext';
+import api from '../../utility/api';
 
 export default function RegisterForm() {
-    const [form, setForm] = useState({ username: '', email: '', password: ''});
-    const { login } = useAuth
+    const [form, setForm] = useState({ username: '', email: '', password: '' });
+    const { login } = useAuth()
+    const navigate =  useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const res = await api.post('/user/register', form);
             login(res.data.token);
-            // navgitate('/') Not sure where I will route this to 
-        } catch (err){
+            navigate('/login') 
+        } catch (err) {
             console.error(err)
             alert("Registration failed")
         }
     }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input
-            type="text"
-            name="username"
-            placeholder="username"
-            value={form.username}
-            onChange=/>
-        
+                type="text"
+                name="username"
+                placeholder="username"
+                value={form.username}
+                onChange={(e) => setForm({ ...form, username: e.target.value })}
+                required />
+            <input
+                type="email"
+                name="email"
+                placeholder="email"
+                value={form.email}
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                required />
+            <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={form.password}
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+                required />
+            <button type ="submit">Register</button>
         </form>
     )
 }
